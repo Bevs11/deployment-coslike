9cfa942e0c55abdbd0aa48fd16d0f877a3972e50
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-06-27 17:09:39 +0800

    fix: changed firebase api keys

--- client/src/firebase.jsx
+++ client/src/firebase.jsx
@@ -1,18 +1,14 @@
-const dotenv = require("dotenv");
-
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
-dotenv.config();
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
+  apiKey: "AIzaSyATwh6Zw7-fG5lcbaPJUx21mmIWSzwDurU",
   authDomain: "coslike-uploads.firebaseapp.com",
   projectId: "coslike-uploads",
   storageBucket: "coslike-uploads.appspot.com",
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  messagingSenderId: "176126108349",
+  appId: "1:176126108349:web:503615f82f18d11df14dd4",
 };
 
 // Initialize Firebase
