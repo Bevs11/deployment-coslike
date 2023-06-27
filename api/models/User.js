6bd40e7dc26aad496a5f78c433200e7d7b80d5ef
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-06-28 07:43:36 +0800

    fix: fixed registration form

--- api/models/User.js
+++ api/models/User.js
@@ -1,54 +1,55 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        require: true,
-        min: 3,
-        max: 20,
-        unique: true
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      min: 3,
+      max: 20,
     },
     email: {
-        type: String,
-        require: true,
-        max: 50,
-        unique: true
+      type: String,
+      require: true,
+      max: 50,
     },
     password: {
-        type: String,
-        required: true,
-        min: 6
+      type: String,
+      required: true,
+      min: 6,
     },
     profilePicture: {
-        type: String,
-        default: ""
+      type: String,
+      default: "",
     },
     coverPicture: {
-        type: String,
-        default: ""
+      type: String,
+      default: "",
     },
     followers: {
-        type: Array,
-        default: []
+      type: Array,
+      default: [],
     },
     following: {
-        type: Array,
-        default: []
+      type: Array,
+      default: [],
     },
     isAdmin: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
     desc: {
-        type: String,
-        max: 50
+      type: String,
+      max: 50,
+      default: "",
     },
     location: {
-        type: String,
-        max: 50
-    }
-},
-{timestamps: true}
+      type: String,
+      max: 50,
+      default: "",
+    },
+  },
+  { timestamps: true }
 );
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
